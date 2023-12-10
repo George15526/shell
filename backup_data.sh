274a90cc27fafe081b0b45300f8f707ca9cd1867
George <george920102@gmail.com>
2023-12-11 03:48:06 +0800

    Debug the date

--- backup_data.sh
+++ backup_data.sh
@@ -18,7 +18,8 @@ mkdir -p /home/george/log_history/log.txt
 log_file="/home/george/log_history/log.txt"
 
 # 記錄執行時間
-echo "[$(date)] Starting backup process" >> "$log_file"
+current_date=$(date "+西元％Y年％m月%d日 (%A) %H時%M分%S秒 %Z")
+echo "[$(current_date)] Starting backup process" >> "$log_file"
 
 # 執行備份命令
 $backup_command >> "$log_file" 2>&1
@@ -26,9 +27,9 @@ $backup_command >> "$log_file" 2>&1
 # 檢查備份命令的執行結果
 if [ $? -eq 0 ]
 then
-  echo "[$(date)] Backup completed successfully" >> "$log_file"
+  echo "[$(current_date)] Backup completed successfully" >> "$log_file"
 else
-  echo "[$(date)] Error: Backup failed" >> "$log_file"
+  echo "[$(current_date)] Error: Backup failed" >> "$log_file"
 fi
 # 壓縮成.tgz檔
 tar -zcvf ${backup_folder}.tgz ${backup_folder}
